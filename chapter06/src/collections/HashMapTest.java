cafe2b3cfa3c5aacb4929cd1486acce1f6097fcc
Sebastian Apel <marloto@codestack.de>
2021-05-12 09:47:17 +0200

    Veranstaltung vom 12.05.2021

---
+++ chapter06/src/collections/HashMapTest.java
@@ -0,0 +1,48 @@
+package collections;
+
+import java.util.HashMap;
+
+public class HashMapTest extends Messwerkzeuge {
+    public static void main(String[] args) {
+        HashMap<Data, String> hashMap = new HashMap<>();
+        // ---------------- Hinzufügen ----------------------------
+        start();
+        for (int i = 0; i < ITERATIONS; i++) {
+            // Fügen Sie das den Wert value unter Verwendung von key 
+            // in die Datenstruktur
+            Data key = dummy(i);
+            String value = dummyData(i);
+            // einfaches add geht nicht, da zwei Werte hinzumüssen
+            hashMap.put(key, value);
+        }
+        stopAndPrint("Hinzufügen: %fns");
+
+        // ------------------ Element enthalten -------------------
+        start();
+        for (int i = 0; i < ITERATIONS; i++) {
+            Data key = dummy(i);
+            // Prüfen Sie ob der Schlüssel in der Menge enthalten ist
+            hashMap.containsKey(key);
+        }
+        stopAndPrint("Element enthalten: %fns");
+
+        // -------------- Iterieren (mit for-each) ----------------
+        start();
+        for (Data obj : hashMap.keySet()) {
+            // ... hier ist nichts zu tun, betrachten Sie jedoch das Ergebnis
+            System.out.println(obj);
+        }
+        stopAndPrint("Iterieren (mit for-each): %fns");
+
+        // --------------------- Löschen --------------------------
+        HashMap<Data, String> copyMap = new HashMap<>(hashMap);
+        start();
+        for (int i = 0; i < ITERATIONS; i++) {
+            Data key = dummy(i);
+            // Entfernen Sie den Schlüssel key aus der Datenstruktur
+            copyMap.remove(key);
+        }
+        stopAndPrint("Löschen: %fns");
+    }
+
+}
---
+++ chapter06/src/collections/LinkedListTest.java
@@ -0,0 +1,73 @@
+package collections;
+
+import java.util.LinkedList;
+
+public class LinkedListTest extends Messwerkzeuge {
+
+    public static void main(String[] args) {
+        LinkedList<Data> linkedList = new LinkedList<>();
+        // ---------------- Hinzufügen am Anfang ------------------
+        // Hinzufügen ans Ende
+        start();
+        for (int i = 0; i < ITERATIONS; i++) {
+            Data dummy = dummy(i);
+            // Fügen Sie das Objekt dummy zum Anfang der ArrayList hinzu
+            linkedList.add(0, dummy);
+        }
+        stopAndPrint("Hinzufügen am Anfang: %fns");
+
+        // --------------- Setzen an Position ---------------------
+        start();
+        for (int i = 0; i < ITERATIONS; i++) {
+            Data dummy = dummy(i);
+            int zufallsIndex = (int) (Math.random() * linkedList.size());
+            // Setzen Sie das Objekt dummy in der ArrayList and die Position zufallsIndex
+            // hinzu
+            linkedList.set(zufallsIndex, dummy);
+        }
+        stopAndPrint("Setzen an Position: %fns");
+
+        // -------------- Letztes Element auslesen ----------------
+        start();
+        for (int i = 0; i < ITERATIONS; i++) {
+            // Beziehen Sie das letzte Element in der ArrayList
+            linkedList.getLast();
+        }
+        stopAndPrint("Letztes Element auslesen: %fns");
+
+        // -------------- Iterieren (mit for-each) ----------------
+        start();
+        for (Data obj : linkedList) {
+            // ... hier ist nichts zu tun
+            System.out.println(obj);
+        }
+        stopAndPrint("Iterieren (mit for-each): %fns");
+
+        // --------------- Iterieren (mit for) --------------------
+        start();
+        for (int i = 0; i < linkedList.size(); i++) {
+            // Beziehen Sie das Element in der ArrayList an der Position i
+            linkedList.get(i);
+        }
+        stopAndPrint("Iterieren (mit for): %fns");
+
+        // --------------- Löschen vom Ende -----------------------
+        LinkedList<Data> copyList = new LinkedList<>(linkedList);
+        start();
+        for (int i = 0; i < ITERATIONS; i++) {
+            // Entfernen Sie das letzte Element
+            copyList.removeLast();
+        }
+        stopAndPrint("Löschen vom Ende: %fns");
+
+        // ------------- Löschen mit Position ---------------------
+        copyList = new LinkedList<>(linkedList);
+        start();
+        for (int i = 0; i < ITERATIONS; i++) {
+            int zufallsIndex = (int) (Math.random() * copyList.size());
+            // Entfernen Sie das Element an Position zufallsIndex
+            copyList.remove(zufallsIndex);
+        }
+        stopAndPrint("Löschen mit Position: %fns");
+    }
+}
