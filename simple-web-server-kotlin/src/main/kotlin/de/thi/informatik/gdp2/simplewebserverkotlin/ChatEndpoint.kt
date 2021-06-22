cda9a113d6c063578fd0fb8bab768848c27fec46
Sebastian Apel <seb@Sebastians-MacBook-Pro.local>
2021-06-22 11:44:13 +0200

    clean kotlin example

--- simple-web-server-kotlin/src/main/kotlin/de/thi/informatik/gdp2/simplewebserverkotlin/ChatEndpoint.kt
+++ simple-web-server-kotlin/src/main/kotlin/de/thi/informatik/gdp2/simplewebserverkotlin/ChatEndpoint.kt
@@ -1,25 +1,5 @@
 package de.thi.informatik.gdp2.simplewebserverkotlin
 
-import de.thi.informatik.gdp2.simplewebserverkotlin.model.Message
-import de.thi.informatik.gdp2.simplewebserverkotlin.model.Rooms
-import org.springframework.web.bind.annotation.*
-import java.util.stream.Collectors
+class ChatEndpoint {
 
-@RestController
-class ChatEndpoint(private val rooms: Rooms) {
-
-    @GetMapping("/hello")
-    fun doSomething(): String = "Hello, World"
-
-    @GetMapping("/rooms")
-    fun listRooms(): Array<String> = rooms.getRooms().toTypedArray()
-
-    @GetMapping("/room/{name}/{since}")
-    fun listMessages(@PathVariable name: String, @PathVariable since: Long): Array<String> =
-        rooms.getRoom(name)!!.getMessagesSince(since).stream().map { msg -> msg.toString() }
-            .collect(Collectors.toList()).toTypedArray()
-
-    @PostMapping("/room/{name}/{user}")
-    fun addMessage(@PathVariable name: String, @PathVariable user: String, @RequestBody message: String) =
-        rooms.getRoom(name)!!.addMessage(user, message)
 }
--- simple-web-server-kotlin/src/main/kotlin/de/thi/informatik/gdp2/simplewebserverkotlin/model/Rooms.kt
+++ simple-web-server-kotlin/src/main/kotlin/de/thi/informatik/gdp2/simplewebserverkotlin/model/Rooms.kt
@@ -1,8 +1,5 @@
 package de.thi.informatik.gdp2.simplewebserverkotlin.model
 
-import org.springframework.stereotype.Component
-
-@Component
 class Rooms {
     private val map: MutableMap<String, Room> = HashMap<String, Room>()
 
